ed064fe037be238c256efde6ad0098621500fd84
Yamashiro Norihiro <e16005@std.it-college.ac.jp>
2016-10-11 21:09:52 +0900

    教科書のP350計算式

--- test/calc.php
+++ test/calc.php
@@ -1,1 +1,28 @@
-P350
+<?php
+$addition = 3 + 8;
+echo $addition;
+echo "<br>";
+
+$subtraction = 6 - 2;
+echo $subtraction;
+echo "<br>";
+
+$multiplication = 4 * 7;
+echo $multiplication;
+echo "<br>";
+
+$division = 12 / 4;
+echo $division;
+echo "<br>";
+
+$surplus = 5 % 3;
+echo $surplus;
+echo "<br>";
+echo "<hr>";
+
+//変数に値を格納した状態で計算
+$a = 3;
+$b = 8;
+$c = $a + $b;
+echo $c;
+?>
